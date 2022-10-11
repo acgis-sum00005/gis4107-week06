8b2c0916df5921022ce08a945148a83cc48e44e4
Megan Fuller <full0085@algonquinlive.com>
2022-10-11 16:46:53 -0400

    #3 first attempt

--- AlvinS_MeganF/phone_utils.py
+++ AlvinS_MeganF/phone_utils.py
@@ -1,7 +1,12 @@
 #valid phone numbers are NNN-NNN-NNNN
+phone_number = 600-600-6000
 
 def is_valid_phone_number(phone_number):
-    phone_number = "600-600-6000"
-    x = phone_number.split("-")
-    print(x)
+    #assess if p_n is 12 digits
+        #assess if dashes are in right position
+            # use .replace and assess .isdigit
+   if len(phone_number) == 12:
+        return True
+    else:
+        return False
 
